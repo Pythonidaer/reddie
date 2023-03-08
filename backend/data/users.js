6f79c8202dfae8fd6710eed0245c1052852345bb
Pythonidaer <pythonidaer@gmail.com>
2023-03-07 21:48:17 -0500

    successfully seeded data to MongoDB, but broke the layout. Because fetch no longer retrieves from the correct id, and the posts id was not imported to the database

--- backend/data/users.js
+++ backend/data/users.js
@@ -2,18 +2,18 @@ import bcrypt from 'bcryptjs'
 
 const users = [
   {
-    name: 'Jonathan Hammond',
+    username: 'Jonathan Hammond',
     email: 'codefolio.work@gmail.com',
     password: bcrypt.hashSync('Nana.2013', 10),
     isAdmin: true,
   },
   {
-    name: 'John Doe',
+    username: 'John Doe',
     email: 'john@example.com',
     password: bcrypt.hashSync('123456', 10),
   },
   {
-    name: 'Jane Doe',
+    username: 'Jane Doe',
     email: 'jane@example.com',
     password: bcrypt.hashSync('123456', 10),
   },
