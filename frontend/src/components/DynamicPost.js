0f3c95842a7e669cabbc7294326c10fd7597f905
Pythonidaer <pythonidaer@gmail.com>
2023-03-26 21:48:34 -0400

    Chat GBT-3 provided recursive functionality that allowed me to fetch up to thousands of comments from Reddits API. Next time I work on this I will need to work on looping this information through components.

--- frontend/src/components/DynamicPost.js
+++ frontend/src/components/DynamicPost.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Card } from 'react-bootstrap'
+import axios from 'axios'
+
 // will need conversion from reddit markdown syntax to html
 const DynamicPost = ({ post, url }) => {
   const seePostsOrComments = (obj, url) => {
@@ -10,6 +12,56 @@ const DynamicPost = ({ post, url }) => {
     const lastUrlPart = urlParts[urlParts.length - 1]
     // Check if the object's key value matches the last part of the URL
     return obj.id === lastUrlPart
+  }
+
+  // Use this function to log all child comments, but will need re-working
+  async function getComments(childId) {
+    const url = `https://www.reddit.com/comments/${post.id}/comment/${childId}.json`
+    try {
+      const response = await axios.get(url)
+      const comments = response.data[1].data.children
+      logAllComments(comments)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  let commentLine = 1
+  // This option works up until the point of the children data structure changing
+  function logAllComments(comments, level = 0) {
+    comments.forEach((comment) => {
+      if (comment.data.body !== undefined) {
+        // skip logging undefined comment bodies
+        const dashes = '-'.repeat(level) // generate dashes for indentation
+        console.log(`${dashes}${comment.data.body}`, commentLine)
+        commentLine++
+      }
+
+      if (
+        comment.kind === 't1' &&
+        comment.data.replies !== '' &&
+        comment.data.replies !== undefined
+      ) {
+        logAllComments(comment.data.replies.data.children, level + 1) // increase level for child comments
+      } else if (comment.kind === 'more') {
+        comment.data.children.forEach((childId) => {
+          getComments(childId)
+        })
+      }
+    })
+  }
+
+  //The post.url just needs '/.json?limit=1' added to fetch a comment
+  // I want to console log
+  const handleClick = async () => {
+    console.log(`${post.url}.json?limit=1`)
+    try {
+      const response = await axios.get(`${post.url}.json?limit=5`)
+      const { children } = response.data[1].data
+      logAllComments(children)
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   return (
@@ -31,7 +83,14 @@ const DynamicPost = ({ post, url }) => {
             </Link>
           </div>
         ) : (
-          <p>Comments</p>
+          <Link
+            className='card-link'
+            to={post.url}
+            target='_blank'
+            onClick={handleClick}
+          >
+            {post.subreddit}
+          </Link>
         )}
       </Card.Body>
     </Card>
@@ -39,25 +98,3 @@ const DynamicPost = ({ post, url }) => {
 }
 
 export default DynamicPost
-/*
-        {!seePostsOrComments(post, url) ? (
-          <div>
-            <Link className='card-link' to={`/dynamicPost/${post.id}`}>
-              See Post
-            </Link>
-            <Link className='card-link' to={post.url} target='_blank'>
-              {post.subreddit}
-            </Link>
-          </div>
-        ) : (
-          <p>Comments</p>
-        )}
-        */
-
-/*
-                <Link className='card-link' to={`/dynamicPost/${post.id}`}>
-          See Post
-        </Link>
-        <Link className='card-link' to={post.url} target='_blank'>
-          {post.subreddit}
-        </Link>*/
