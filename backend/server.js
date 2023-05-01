e5d05e638b399a8d4e8ee58084d7c969b2db4b02
Pythonidaer <pythonidaer@gmail.com>
2023-04-30 21:39:44 -0400

    Modifying server script for Heroku PRD build

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
@@ -22,6 +23,21 @@ app.get('/', (req, res) => {
 // Routes
 app.use('/api/users', userRoutes)
 app.use('/api/comments', commentRoutes)
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+  // FIX: below code fixes app crashing on refresh in deployment
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+  })
+} else {
+  app.get('/', (_, res) => {
+    res.status(200).json({ message: 'Welcome to the Reddie API' })
+  })
+}
 
 // Error Handling Middleware
 app.use(notFound)
